0bb06bf95a47dea7f8b4d1146e35331e77fbc5a2
SiCong Li <sicong.li@arm.com>
2019-01-10 15:45:50 +0000

    Add scripts for scaling study

---
+++ Matrix-Vector-Multiplication/test_scaling.sh
@@ -0,0 +1,54 @@
+NUM_REP=10
+
+# Args
+# $1 - Number of processes
+# $2 - Number of repetitions
+# $3 - ARow
+# $4 - ACol
+function record {
+  local rep_count;
+  rep_count=0;
+  record_str="$1,$3,$4,";
+  while [ ${rep_count} -le $2 ]; do
+    record_str+="$(TIMEFORMAT="%3R"; time (mpirun -n $1 ./main $3 $4) 2>&1),";
+    rep_count=$(( rep_count + 1 ));
+  done
+  printf "${record_str}\n";
+}
+
+function test_strong_scaling {
+  AROW=8192
+  ACOL=8192
+  # Number of processes = 1 (Serial)
+  record 1 ${NUM_REP} ${AROW} ${ACOL};
+  # Number of processes = 2
+  record 2 ${NUM_REP} ${AROW} ${ACOL};
+  # Number of processes = 4
+  record 4 ${NUM_REP} ${AROW} ${ACOL};
+  # Number of processes = 8
+  record 8 ${NUM_REP} ${AROW} ${ACOL};
+}
+
+function test_weak_scaling {
+  # Number of processes = 1 (Serial)
+  AROW=2048
+  ACOL=2048
+  record 1 ${NUM_REP} ${AROW} ${ACOL};
+  # Number of processes = 2
+  AROW=4096
+  ACOL=4096
+  record 2 ${NUM_REP} ${AROW} ${ACOL};
+  # Number of processes = 4
+  AROW=8192
+  ACOL=8192
+  record 4 ${NUM_REP} ${AROW} ${ACOL};
+  # Number of processes = 8
+  AROW=16384
+  ACOL=16384
+  record 8 ${NUM_REP} ${AROW} ${ACOL};
+}
+
+# Main
+cmd=$1
+shift
+${cmd} "$@"
